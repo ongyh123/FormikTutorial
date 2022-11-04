538217d53f07d9bf0917a2c967d965a67564cfe4
ongyh123 <105908156+ongyh123@users.noreply.github.com>
2022-11-04 10:54:42 +0800

    setting up

---
+++ src/components/BasicForm.js
@@ -0,0 +1,25 @@
+import { useFormik } from 'formik';
+
+const BasicForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+    },
+  });
+
+  console.log(formik);
+
+  return (
+    <form autoComplete='off'>
+      <label htmlFor='email'>Email</label>
+      <input
+        value={formik.value.email}
+        onChange={formik.handleChange}
+        id='email'
+        type='email'
+        placeholder='Enter your email'
+      />
+    </form>
+  );
+};
+export default BasicForm;
