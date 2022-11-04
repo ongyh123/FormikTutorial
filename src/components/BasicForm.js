68c18d3dc323802040111e584bfa8dee8cf44d5f
ongyh123 <105908156+ongyh123@users.noreply.github.com>
2022-11-04 11:08:12 +0800

    setting up formik

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -4,6 +4,9 @@ const BasicForm = () => {
   const formik = useFormik({
     initialValues: {
       email: '',
+      age: '',
+      password: '',
+      confirmPassword: '',
     },
   });
 
@@ -13,12 +16,41 @@ const BasicForm = () => {
     <form autoComplete='off'>
       <label htmlFor='email'>Email</label>
       <input
-        value={formik.value.email}
-        onChange={formik.handleChange}
         id='email'
         type='email'
         placeholder='Enter your email'
+        value={formik.values.email}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
       />
+      <label htmlFor='email'>Age</label>
+      <input
+        id='age'
+        type='number'
+        placeholder='Enter your age'
+        value={formik.values.age}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      <label htmlFor='email'>Password</label>
+      <input
+        id='password'
+        type='password'
+        placeholder='Enter your password'
+        value={formik.values.password}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      <label htmlFor='email'>Confirm Password</label>
+      <input
+        id='confirmPassword'
+        type='password'
+        placeholder='Confirm Password'
+        value={formik.values.confirmPassword}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      <button type='submit'>Submit</button>
     </form>
   );
 };
